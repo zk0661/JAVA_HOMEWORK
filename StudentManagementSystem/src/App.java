a94326e68e35f0d4b9893a5665dfdcdf9a18e330
linuk <zk84377361@outlook.com>
2022-10-18 20:36:38 +0800

    Student Management System update

--- StudentManagementSystem/src/App.java
+++ StudentManagementSystem/src/App.java
@@ -1,7 +1,21 @@
+import java.io.*;
 import java.util.HashMap;
 
 public class App {
-    public static void main(String[] args) {
-        System.out.println("hello world!");
+    public static void main(String[] args) throws IOException {
+        FileReader fileReader = new FileReader("lib/Student.txt");
+        System.out.println(fileReader.toString());
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        String str = new String();
+        str = bufferedReader.readLine();
+        String[] stt = str.split(" ");
+        for (int i = 0; i < stt.length; i++) {
+            System.out.println(stt[i]);
+            System.out.println(Integer.parseInt(stt[4])+1);
+        }
+        bufferedReader.close();
+        fileReader.close();
+
     }
+
 }
--- StudentManagementSystem/src/ClassSet.java
+++ StudentManagementSystem/src/ClassSet.java
@@ -1,3 +1,4 @@
+import java.io.*;
 import java.util.HashMap;
 
 public class ClassSet {
@@ -5,38 +6,79 @@ public class ClassSet {
     HashMap<String, String> NameTable;
     // 从文件中读取数据构造hashmap
 
-    public ClassSet() {
+    public ClassSet() throws IOException {
         // 遍历每一个元素
         ImportStudentInformation();
-        assert false;
+        // 构建姓名-学号索引表
         for (String o : StudentTable.keySet()) {
-            NameTable.put(StudentTable.get(o).getSname(), o);
+            if (StudentTable.get(o) != null) {
+                NameTable.put(StudentTable.get(o).getSname() + "#", StudentTable.get(o).getSnumber());
+            }
+            else {
+                NameTable.put(StudentTable.get(o).getSname(), o);
+            }
         }
-
     }
 
-    private void ImportStudentInformation() {
-
+    private void ImportStudentInformation() throws IOException {
+        FileReader fileReader = new FileReader("lib/Student.txt");
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        String Line;
+        String[] Message;
+        Student temp = new Student();
+        while ((Line = bufferedReader.readLine()) != null) {
+            Message = Line.split(" ");
+            temp.setSnumber(Message[0]);
+            temp.setSex(Message[1].charAt(0));
+            temp.setSname(Message[2]);
+            temp.setAge(Integer.parseInt(Message[3]));
+            temp.setScore(Integer.parseInt(Message[4]));
+            StudentTable.put(temp.getSnumber(), temp);
+        }
+        bufferedReader.close();
+        fileReader.close();
     }
 
     // 将学生信息写回文件
-    private void ExportStudentInformation() {
+    private void ExportStudentInformation() throws IOException {
+        FileWriter fileWriter = new FileWriter("lib/Student.txt");
+        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+        bufferedWriter.close();;
+        fileWriter.close();
+    }
 
+    public void UpdateNameTable(Student o) {
+        while (NameTable.get(o.getSname()) != null) {
+            o.setSname(o.getSname() + "#");
+        }
+        NameTable.put(o.getSname(), o.getSnumber());
     }
 
     // 增加一个学生
     public boolean Additions(Student o) {
-        return false;
+        if (StudentTable.get(o.getSnumber()) != null) {
+            return false;
+        }
+        StudentTable.put(o.getSnumber(), o);
+        // 更新姓名-学号索引表
+        UpdateNameTable(o);
+        return true;
     }
 
     // 修改学生信息
     public boolean Modify(Student o) {
-        return false;
+        // 不允许修改学号
+        if (StudentTable.get(o.getSnumber()) == null) {
+            return false;
+        }
+        StudentTable.put(o.getSnumber(), o);
+        UpdateNameTable(o);
+        return true;
     }
 
     // 删除学生
-    public boolean Delete(Student o) {
-        return false;
+    public void Delete(Student o) {
+        StudentTable.remove(o.getSnumber());
     }
 
     public int getStudentSum() {
