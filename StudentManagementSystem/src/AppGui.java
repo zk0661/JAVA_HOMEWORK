e119cca261623c504eb66b01cd0071495aefe74d
linuk <zk84377361@gmail.com>
2022-10-19 17:24:59 +0800

    Update

--- StudentManagementSystem/src/AppGui.java
+++ StudentManagementSystem/src/AppGui.java
@@ -1,10 +1,17 @@
-import javax.swing.JFrame;
+import javax.swing.*;
 
 public class AppGui {
+
+    private JLabel jLabel;
+    private JFrame jFrame;
+    private JButton jButton;
+    private JPanel jPanel;
 
     // 实现与ClassSet与界面的连接，应提供接口
     public AppGui() { // 
         JFrame jFrame = new JFrame();
+//        JLabel jLabel = new JLabel();
+
         jFrame.setSize(620, 400);
         jFrame.setVisible(true);
     }
