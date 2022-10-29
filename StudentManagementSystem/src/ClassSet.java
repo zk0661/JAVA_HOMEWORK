4902259cf39b272b1e57b919ebed482e5d97f698
linuk <zk84377361@gmail.com>
2022-10-29 23:01:47 +0800

    20221029 update

--- StudentManagementSystem/src/ClassSet.java
+++ StudentManagementSystem/src/ClassSet.java
@@ -1,22 +1,57 @@
 import java.io.*;
 import java.util.HashMap;
+import java.util.Vector;
 
 public class ClassSet {
     HashMap<String, Student> StudentTable;
-    HashMap<String, String> NameTable;
+    HashMap<String, String> NameTable; // 姓名-学号表，实现按名查找
     // 从文件中读取数据构造hashmap
-
     public ClassSet() throws IOException {
         // 遍历每一个元素
+        StudentTable = new HashMap<String, Student>();
+        NameTable = new HashMap<String, String>();
         ImportStudentInformation();
         // 构建姓名-学号索引表
-        for (String o : StudentTable.keySet()) {
-            if (StudentTable.get(o) != null) {
-                NameTable.put(StudentTable.get(o).getSname() + "#", StudentTable.get(o).getSnumber());
+        Rebuild();
+        NameNumberMap();
+    }
+
+    private void Rebuild() {
+        HashMap<String, Integer> stringHashMap = new HashMap<String, Integer>();
+        for (String o : StudentTable.keySet()) { // 遍历学号，出现同名的学生自动加‘#’
+            String ss = StudentTable.get(o).getSname();
+            if (stringHashMap.get(ss) == null) {
+                stringHashMap.put(ss, 1);
             }
             else {
-                NameTable.put(StudentTable.get(o).getSname(), o);
+                stringHashMap.put(ss, stringHashMap.get(ss) + 1);
+                int temp = stringHashMap.get(ss);
+                // 增加 temp - 1 个 '#'
+                String temp2 = ss + "#".repeat(Math.max(0, temp - 1));
+                StudentTable.get(o).setSname(temp2);
             }
+        }
+    }
+
+    private void NameNumberMap() {
+        for (String o : StudentTable.keySet()) {
+            NameTable.put(StudentTable.get(o).getSname(), o);
+        }
+    }
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder("{\n");
+        for (String o : StudentTable.keySet()) {
+            if (StudentTable.get(o) != null) {
+                res.append(StudentTable.get(o).toString()).append('\n');
+            }
+        }
+        return res + "}";
+    }
+
+    public void DispNameTable() {
+        for (String o : NameTable.keySet()) {
+            System.out.println(o + "->" + NameTable.get(o));
         }
     }
 
@@ -25,15 +60,15 @@ public class ClassSet {
         BufferedReader bufferedReader = new BufferedReader(fileReader);
         String Line;
         String[] Message;
-        Student temp = new Student();
         while ((Line = bufferedReader.readLine()) != null) {
+            Student temp = new Student();
             Message = Line.split(" ");
             temp.setSnumber(Message[0]);
             temp.setSex(Message[1].charAt(0));
             temp.setSname(Message[2]);
             temp.setAge(Integer.parseInt(Message[3]));
             temp.setScore(Integer.parseInt(Message[4]));
-            StudentTable.put(temp.getSnumber(), temp);
+            this.StudentTable.put(temp.getSnumber(), temp);
         }
         bufferedReader.close();
         fileReader.close();
@@ -43,7 +78,9 @@ public class ClassSet {
     private void ExportStudentInformation() throws IOException {
         FileWriter fileWriter = new FileWriter("lib/Student.txt");
         BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-        bufferedWriter.close();;
+        // 将学生信息表中的信息写回文件
+
+        bufferedWriter.close();
         fileWriter.close();
     }
 
@@ -95,5 +132,18 @@ public class ClassSet {
         return StudentTable.get(NameTable.get(name));
     }
 
+    public Vector<String> getAll() {
+        Vector<String> res = new Vector<String>();
+        for (String o : StudentTable.keySet()) {
+            Student temp = StudentTable.get(o);
+            String temp2 = temp.getSnumber() + ' ' +
+                           temp.getSex() + ' ' +
+                           temp.getSname() + ' ' +
+                           temp.getAge() + ' ' +
+                           temp.getScore();
+            res.add(temp2);
+        }
+        return res;
+    }
 
 }
